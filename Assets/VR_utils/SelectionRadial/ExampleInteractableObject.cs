26cb4176ae3670b1b30d655acb61696efabf043d
Pastew <kpastew@gmail.com>
2018-11-29 15:48:45 +0100

    Refactor

--- Assets/VR_utils/SelectionRadial/ExampleInteractableObject.cs
+++ Assets/VR_utils/SelectionRadial/ExampleInteractableObject.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Cube : Interactable
+public class ExampleInteractableObject : Interactable
 {
     float spinValue = 90;
 
