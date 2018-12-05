eca2ed4ae4018fe11ab5879c0210c20d396215ea
Pastew <kpastew@gmail.com>
2018-12-05 22:59:19 +0100

    added doors script,  enabled anti aliasing

---
+++ Assets/Doors.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doors : Interactable {
+
+    int closed = 1;
+
+    protected override void InvokeAction()
+    {
+        transform.Rotate(Vector3.up, 90 * closed);
+        closed *= -1;
+    }
+}
