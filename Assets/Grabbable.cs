1e2c1263df5c881771c50f6c753ae0818102d0d9
Pastew <kpastew@gmail.com>
2018-12-09 00:32:37 +0100

    Grabbing objects works

---
+++ Assets/Grabbable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grabbable : Interactable
+{
+
+    private Transform originalParent;
+    private Vector3 originalLocalPosition;
+
+    private void Awake()
+    {
+        originalParent = transform.parent;
+        originalLocalPosition = transform.localPosition;
+    }
+
+    protected override void InvokeAction()
+    {
+        Transform hand = FindObjectOfType<Hand>().transform;
+        transform.parent = hand;
+        transform.localPosition = Vector3.zero;
+    }
+
+}
