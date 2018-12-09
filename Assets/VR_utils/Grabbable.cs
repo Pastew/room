d9d23aae227ea3ee274cd43dfe74bab830f0525e
Pastew <kpastew@gmail.com>
2018-12-09 01:17:28 +0100

    Putting back objects works

--- Assets/VR_utils/Grabbable.cs
+++ Assets/VR_utils/Grabbable.cs
@@ -1,12 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Grabbable : Interactable
 {
-
     private Transform originalParent;
     private Vector3 originalLocalPosition;
+    private GameObject putDownPlace;
 
     private void Awake()
     {
@@ -14,8 +15,22 @@ public class Grabbable : Interactable
         originalLocalPosition = transform.localPosition;
     }
 
+    internal void PutBackOnPlace()
+    {
+        transform.parent = originalParent;
+        transform.localPosition = originalLocalPosition;
+    }
+
+    private void Start()
+    {
+        putDownPlace = Resources.Load("PutDownPlace") as GameObject;
+    }
+
     protected override void InvokeAction()
     {
+        GameObject place = Instantiate(putDownPlace, originalParent, false);
+        place.transform.localPosition = originalLocalPosition;
+
         Transform hand = FindObjectOfType<Hand>().transform;
         transform.parent = hand;
         transform.localPosition = Vector3.zero;
---
+++ Assets/VR_utils/PutDownPlace.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PutDownPlace : Interactable
+{
+    protected override void InvokeAction()
+    {
+        Hand hand = FindObjectOfType<Hand>();
+        GameObject heldObject = hand.transform.GetChild(0).gameObject;
+        heldObject.GetComponent<Grabbable>().PutBackOnPlace();
+        Destroy(gameObject);
+    }
+}
