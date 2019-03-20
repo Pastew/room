965915013f296527eee53ee74db2c82e61fc06ae
Pastew <kpastew@gmail.com>
2019-03-20 23:29:51 +0100

    Fix VRLook moving

--- Assets/Scripts/VRLookWalk.cs
+++ Assets/Scripts/VRLookWalk.cs
@@ -3,28 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
-public class VRLookWalk : MonoBehaviour {
+public class VRLookWalk : MonoBehaviour
+{
 
     public Transform vrCamera;
 
-    public float toggleAngle = 25.0f;
-    public float maxSpeed = 3.0f;
-    public float speedIncrease = 50f;
+    public float toggleAngle = 65.0f;
+    public float maxSpeed = 1.0f;
+    public float speedIncrease = 2.5f;
 
     private float speed;
     public bool moveForward;
 
     private CharacterController characterControlleer;
 
-	void Start () {
+    void Start()
+    {
         characterControlleer = GetComponent<CharacterController>();
-        if (vrCamera == null) {
+        if (vrCamera == null)
+        {
             Debug.LogWarning("Camera transform is not set. I will use main camera");
             vrCamera = Camera.main.transform;
         }
-	}
-	
-	void Update () {
+    }
+
+    void Update()
+    {
         if (vrCamera.eulerAngles.x >= toggleAngle && vrCamera.eulerAngles.x < 90.0f)
         {
             speed += Time.deltaTime * speedIncrease;
@@ -36,8 +40,8 @@ public class VRLookWalk : MonoBehaviour {
 
         speed = Mathf.Clamp(speed, 0, maxSpeed);
 
-        Vector3 forwardDirection = vrCamera.transform.TransformDirection(Vector3.forward);
-        characterControlleer.SimpleMove(forwardDirection * speed);
-
+        Vector3 cameraCorwardDirection = vrCamera.transform.forward.normalized;
+        Vector3 characterControllerMoveDirection = new Vector3(cameraCorwardDirection.x, 0, cameraCorwardDirection.z).normalized;
+        characterControlleer.SimpleMove(characterControllerMoveDirection * speed);
     }
 }
